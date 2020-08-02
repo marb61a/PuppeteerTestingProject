0ca958658d27e391cdd267a1ae17cbe1f0ae8dff
Martin <marb61a@gmail.com>
2020-08-02 20:21:06 +0100

    Updated second test file

--- tests/exampleb.test.js
+++ tests/exampleb.test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 describe('The third set of basic testing examples', () => {
-    it('should launch the browser', () => {
+    it('should launch the browser', async function() {
         const browser = await puppeteer.launch({
             headless: false,
             slowMo: 150,
@@ -13,7 +13,27 @@ describe('The third set of basic testing examples', () => {
         });
         const page = await browser.newPage();
 
-        await page.goto('https://devexpress.github.io/testcafe/example/');
+        await page.goto('http://example.com');
+
+        // Getting the title of the page
+        const title = await page.title();
+
+        // Getting the page url
+        const url = await page.url();
+
+        // console.log('Title: ', title);
+        // console.log('URL : ', url);
+
+        // Getting text from an element
+        // Element is a callback function which will return the element text content
+        const text = await page.$eval('h1', element => element.textContent);
+        // console.log('H1 text is : ', text);
+
+        // Counting elements on a page
+        // Single dollar signs on eval is for single elements
+        // for multiple elements use 2 dollar signs
+        const count = await page.$$eval('p', element => element.length);
+        console.log('The number of p tags is : ', count);
 
         await browser.close();
     });
