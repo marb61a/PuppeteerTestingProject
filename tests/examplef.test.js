c50fe2803c3a3e67290e139e42d61a97b16fee4a
Martin <marb61a@gmail.com>
2020-08-04 20:01:43 +0100

    refactored tests to use hooks

---
+++ tests/examplef.test.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+const expect = require('chai').expect;
+
+describe('The seventh set of basic testing examples', () => {
+    let browser;
+    let page;
+
+    // Will be ran before any tests
+    before(async function(){
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 150,
+            devtools: false,
+            defaultViewport: null,
+            args: ['--start-maximized']
+        });
+
+        page = await browser.newPage();
+
+        await page.setDefaultTimeout(10000);
+        await page.setDefaultNavigationTimeout(15000);
+    });
+
+    // Will be ran after tests
+    after(async function() {
+        await browser.close();
+    })
+
+    it('should launch the browser', async function() {
+        await page.goto('http://example.com');
+        await page.waitForXPath('//h1');
+
+        const title = await page.title();
+        const url = await page.url();
+        const text = await page.$eval('h1', element => element.textContent);
+        const count = await page.$$eval('p', element => element.length);
+
+        expect(title).to.be.a('string', 'Example Domain');
+        expect(url).to.include('example.com');
+        expect(text).to.be.a('string', 'Example Domain');
+        expect(count).to.equal(2);
+
+        await page.goto('http://zero.webappsecurity.com/index.html');
+        await page.waitForSelector('#signin_button');
+        await page.click('#signin_button');
+        await page.waitFor(() => !document.querySelector('#signin_button'));    
+        await page.waitForSelector('#signin_button', {
+            hidden: true,
+            timeout: 3000
+        });
+    });
+});
