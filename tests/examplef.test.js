c7ef079401b141c32181aac120828386262b9e8b
Martin <marb61a@gmail.com>
2020-08-07 20:00:28 +0100

    Updating helper functions to extend puppeteer

--- tests/examplef.test.js
+++ tests/examplef.test.js
@@ -1,6 +1,10 @@
 const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
 
+const click = require('../lib/helpers');
+const getText = require('../lib/helpers');
+const getCount = require('../lib/helpers');
+
 describe('The seventh set of basic testing examples', () => {
     let browser;
     let page;
@@ -42,8 +46,10 @@ describe('The seventh set of basic testing examples', () => {
 
         const title = await page.title();
         const url = await page.url();
-        const text = await page.$eval('h1', element => element.textContent);
-        const count = await page.$$eval('p', element => element.length);
+        // const text = await page.$eval('h1', element => element.textContent);
+        // const count = await page.$$eval('p', element => element.length);
+        const text = await getText(page, 'h1');
+        const count = await getText(page, 'p');
 
         expect(title).to.be.a('string', 'Example Domain');
         expect(url).to.include('example.com');
@@ -51,8 +57,9 @@ describe('The seventh set of basic testing examples', () => {
         expect(count).to.equal(2);
 
         await page.goto('http://zero.webappsecurity.com/index.html');
-        await page.waitForSelector('#signin_button');
-        await page.click('#signin_button');
+        // await page.waitForSelector('#signin_button');
+        // await page.click('#signin_button');
+        await click(page, '#signin_button');
         await page.waitFor(() => !document.querySelector('#signin_button'));    
         await page.waitForSelector('#signin_button', {
             hidden: true,
