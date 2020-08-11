4b432d98a17a5544c133c47728284ae53bc1bef3
Martin <marb61a@gmail.com>
2020-08-11 20:57:50 +0100

    Updated e2e login test

--- tests/e2e/login.test.js
+++ tests/e2e/login.test.js
@@ -23,11 +23,33 @@ describe('Login Test', () => {
         await browser.close();
     });
 
-    it('Login Test - Invalid Credentials', () => {
+    it('Login Test - Invalid Credentials', async function(){
+        await page.goto('http://zero.webappsecurity.com/index.html');
+        
+        // Always use wait for selector when waiting to click on a selector 
+        await page.waitForSelector('#signin_button');
+        await page.click('#signin_button');
 
+        await page.waitForSelector('#login_form');
+        await page.type('#user_login', 'invalid creds');
+        await page.type('#user_password', 'invalid password');
+        await page.click('#user_remember_me');
+        await page.click('input[type="submit"]');
+        await page.waitForSelector('.alert-error');
     });
 
-    it('Login Test - Valid Credentials', () => {
+    it('Login Test - Valid Credentials', async function(){
+        await page.goto('http://zero.webappsecurity.com/index.html');
+        
+        await page.waitForSelector('#signin_button');
+        await page.click('#signin_button');
+
+        await page.waitForSelector('#login_form');
+        await page.type('#user_login', 'username');
+        await page.type('#user_password', 'password');
+        await page.click('#user_remember_me');
+        await page.click('input[type="submit"]');
+        await page.waitForSelector('#settingsBox');
 
     });
 
