5aed4a9c73ac4282db5b52b29a18ae757c7c568f
Martin <marb61a@gmail.com>
2020-08-13 20:49:06 +0100

    Added initial currency exchange  test

--- tests/e2e/currency-exchange.test.js
+++ tests/e2e/currency-exchange.test.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 
-describe('Payment Test', () => {
+describe('Currency Exchange Test', () => {
     let browser;
     let page;
 
@@ -30,20 +30,11 @@ describe('Payment Test', () => {
         await browser.close();
     });
 
-    it('Display Payment Form', async function(){
-        await page.waitForSelector('.nav-tabs');
-        await page.click('#pay_bills_tab');
-        await page.waitForSelector('.board');
+    it('Display Currency Exchange Form', async function(){
+
     });
 
-    it('Make Payment', async function(){
-        await page.select('#sp_payee', 'Apple');
-        await page.select('#sp_account', 'Credit Card');
-        await page.type('#sp_amount', '500');
-        await page.type('#sp_date', '2020-03-18');
-        await page.keyboard.press('Enter');
-        await page.type('#sp_description', 'Payment for rent');
-        await page.click('#pay_saved_payees');
-        await page.waitForSelector('#alert_content');    
+    it('Exchange Currency', async function(){
+
     });
 });
