013eedd4b5499365049d8bf46dacff493628b7a4
Martin <marb61a@gmail.com>
2020-08-13 20:44:05 +0100

    updated payment test

--- tests/e2e/payment.test.js
+++ tests/e2e/payment.test.js
@@ -17,13 +17,33 @@ describe('', () => {
 
         await page.setDefaultTimeout(10000);
         await page.setDefaultNavigationTimeout(15000);
+
+        await page.goto('http://zero.webappsecurity.com/login.html');
+        await page.waitForSelector('#login_form');
+        await page.type('#user_login', 'username');
+        await page.type('#user_password', 'password');
+        await page.click('#user_remember_me');
+        await page.click('input[type="submit"]');
     });
 
     after(async function() {
         await browser.close();
     });
 
-    it('', async function(){
+    it('Display Payment Form', async function(){
+        await page.waitForSelector('.nav-tabs');
+        await page.click('#pay_bills_tab');
+        await page.waitForSelector('.board');
+    });
 
+    it('Make Payment', async function(){
+        await page.select('#sp_payee', 'Apple');
+        await page.select('#sp_account', 'Credit Card');
+        await page.type('#sp_amount', '500');
+        await page.type('#sp_date', '2020-03-18');
+        await page.keyboard.press('Enter');
+        await page.type('#sp_description', 'Payment for rent');
+        await page.click('#pay_saved_payees');
+        await page.waitForSelector('#alert_content');    
     });
 });
