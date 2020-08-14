0b224b32304770ed2917eceae00874361f6ca82a
Martin <marb61a@gmail.com>
2020-08-14 19:50:29 +0100

    updated currency exchange test

--- tests/e2e/currency-exchange.test.js
+++ tests/e2e/currency-exchange.test.js
@@ -31,10 +31,18 @@ describe('Currency Exchange Test', () => {
     });
 
     it('Display Currency Exchange Form', async function(){
-
+        await page.waitForSelector('.nav-tabs');
+        await page.click('#pay_bills_tab');
+        await page.waitForSelector('#tabs > ul > li:nth-child(3) > a');
+        await page.click('#tabs > ul > li:nth-child(3) > a');
+        await page.waitForSelector('.board');
     });
 
     it('Exchange Currency', async function(){
-
+        await page.select('#pc_currency', 'GBP');
+        await page.type('#pc_amount', '800');
+        await page.click('#pc_inDollars_true');
+        await page.click('#purchase_cash');
+        await page.waitForSelector('#alert_content');
     });
 });
